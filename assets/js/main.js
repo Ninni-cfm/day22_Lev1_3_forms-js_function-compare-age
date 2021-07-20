cd2062459c4d5d6455e89d6db11fe62aac836044
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-07-20 16:33:15 +0200

    Math.abs introduced

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 function getDifference() {
     let year1 = document.getElementById('year1').value;
     let year2 = document.getElementById('year2').value;
+    let result = document.getElementById('result');
 
-    document.getElementById('result').innerHTML = year1 - year2;
+
+    // variant 1: using Math.abs, elegant and short
+    // result.innerHTML = Math.abs(year1 - year2);
+
+    // variant 2: using a conditional operator, still elegant and short
+    // result.innerHTML = year1 > year2 ? year1 - year2 : year2 - year1;
+
+    // variant 3: using if .. else .. , the usual way, but much code...  
+    if (year1 > year2) {
+        result.innerHTML = year1 - year2;
+    } else {
+        result.innerHTML = year2 - year1;
+    }
 }
