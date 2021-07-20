2c35de455dedcecdf1c86155c224d9be27d069c6
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-07-20 15:10:55 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function getDifference() {
+    let year1 = document.getElementById('year1').value;
+    let year2 = document.getElementById('year2').value;
+
+    document.getElementById('result').innerHTML = year1 - year2;
+}
